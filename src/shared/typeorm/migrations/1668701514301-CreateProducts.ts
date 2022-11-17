41c18300f51a074336ff7d7884ae381a21cb8f53
andreapmacedo <aa.andremacedo@gmail.com>
2022-11-17 13:20:57 -0300

    fix: type para timestamp with time zone.

--- src/shared/typeorm/migrations/1668701514301-CreateProducts.ts
+++ src/shared/typeorm/migrations/1668701514301-CreateProducts.ts
@@ -30,12 +30,12 @@ export class CreateProducts1607437608841 implements MigrationInterface {
           },
           {
             name: 'created_at',
-            type: 'timestamp',
+            type: 'timestamp with time zone',
             default: 'now()',
           },
           {
             name: 'updated_at',
-            type: 'timestamp',
+            type: 'timestamp with time zone',
             default: 'now()',
           },
         ],
