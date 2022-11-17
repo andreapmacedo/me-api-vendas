9328cfc894dcb3d5838b0faff771c7ac68c1fde1
andreapmacedo <aa.andremacedo@gmail.com>
2022-11-17 10:14:01 -0300

    feat: Adiciona arquivos para a primeira execução

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,14 @@
-console.log('Hello World');
+import Express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = Express();
+
+app.use(cors());
+app.use(Express.json());
+
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
